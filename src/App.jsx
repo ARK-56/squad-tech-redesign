9591461b823287f5ea756d4aacd2cb29821b66d3
MKashanButt <kashanbutt566@gmail.com>
2026-06-15 23:17:49 +0500

    UPT: next folder to react folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop'
